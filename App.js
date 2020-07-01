45535962f9a7beaacc7de7b9cc1883577029853e
Dayo Akinkuowo <me@sprypixels.com>
2020-07-02 00:20:51 +0100

    initial push

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
 import { WebView } from "react-native-webview";
 
 export default function App() {
-  return (
+  const [isLoaded, setIsLoaded] = useState(true);
+  return isLoaded ? (
     <WebView
       source={{ uri: "https://klcconline.com/" }}
       style={{ paddingTop: 20 }}
       onLoad={(syntheticEvent) => {
         const { nativeEvent } = syntheticEvent;
         console.log("======URL", nativeEvent);
+        setIsLoaded(true);
       }}
     />
+  ) : (
+    <View style={styles.imageWrapper}>
+      <Image
+        source={require("./assets/splash.png")}
+        resizeMode="contain"
+        style={styles.image}
+      />
+    </View>
   );
 }
 
@@ -21,5 +31,17 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  imageWrapper: {
+    width: "100%",
+    height: "100%",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
   },
 });
